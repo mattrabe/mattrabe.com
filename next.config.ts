d9ead63350f028e2b8092c6329d9e12450d4e535
Matt Rabe <mrabe@completecodesolutions.com>
2025-08-29 07:15:20 -1000

    Support 3 build types

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
+const output = (process.env.NEXT_BUILD_TYPE === 'export' && 'export') ||
+  (process.env.NEXT_BUILD_TYPE === 'standalone' && 'standalone') ||
+  undefined
+
 const nextConfig: NextConfig = {
+  output,
   /* config options here */
 };
 
