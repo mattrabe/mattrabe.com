570f038f676899a81ceffefe8b03023f3483b995
Matt Rabe <mrabe@completecodesolutions.com>
2025-09-02 15:02:31 -1000

    Handle mobile on resume page

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -7,20 +7,32 @@ export function Resume(props: {
 }) {
   return (
     <>
-      <Link
-        href='https://docs.google.com/document/d/1aMJ1se0rFbErJs9vFdZ8uDyWZTO6TwweKHZlMlGuy5Q/view'
-        target='_blank'
-        rel='noopener noreferrer'
+      <div
         className='show-on-mobile'
+        style={{
+          padding: '20px',
+          textAlign: 'center',
+        }}
       >
-        Open in Google Docs
-        <FaExternalLinkAlt
-          size={10}
-          style={{ marginLeft: 5 }}
-        />
-      </Link>
+        <p>It looks like you&apos;re on mobile, which means my resume will be best viewed in Google Docs.</p>
+
+        <Link
+  //        href='https://docs.google.com/document/d/e/2PACX-1vSXxS4coPbUnZBDiMXB88Jl1yNbrLgec4havyzQhaE6jRJ5_n5f9Q91guy_OnslUDaEHeh93Y7MGAqf/pub?embedded=true'
+          href='https://docs.google.com/document/d/1aMJ1se0rFbErJs9vFdZ8uDyWZTO6TwweKHZlMlGuy5Q/view'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Open in Google Docs
+          <FaExternalLinkAlt
+            size={10}
+            style={{ marginLeft: 5 }}
+          />
+        </Link>
+      </div>
+
       <iframe
         className='hide-on-mobile'
+  //      src='https://docs.google.com/document/d/e/2PACX-1vSXxS4coPbUnZBDiMXB88Jl1yNbrLgec4havyzQhaE6jRJ5_n5f9Q91guy_OnslUDaEHeh93Y7MGAqf/pub?embedded=true'
         src='https://docs.google.com/document/d/1aMJ1se0rFbErJs9vFdZ8uDyWZTO6TwweKHZlMlGuy5Q/view?embedded=true'
         style={{
           backgroundColor: '#ccc',
