b8fc0053eac5d09a0997357ccbd7a4b52fb743f1
Matt Rabe <mrabe@completecodesolutions.com>
2025-09-04 10:11:39 -1000

    TransistionMaterial POC

--- src/components/Brain/ThreeDBrain/BrainModel/types.ts
+++ src/components/Brain/ThreeDBrain/BrainModel/types.ts
@@ -1,7 +1,14 @@
 import type {
   BufferGeometry,
+  Color,
   Object3D,
 } from 'three'
+
+export type BrainNode = {
+  color?: Color,
+  isTransitionMaterial?: boolean,
+  object3d: Node3D,
+}
 
 export type Node3D = Object3D & {
   geometry: BufferGeometry,
